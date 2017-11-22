ccd0e12319fd4a21ed8fdcf1177150c894076ebc
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2017-11-22 14:33:34 -0500

    index

--- public/index.js
+++ public/index.js
@@ -2,6 +2,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World 5\n');
+  res.end('Hello World 88\n');
 }).listen(8080, '162.243.217.204');
 console.log('Server running at http://162.243.217.204:8080/')
